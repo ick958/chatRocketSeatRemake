c486948132b96a4acee7a18601b2f2cc4daba51b
ick958 <57007684+ick958@users.noreply.github.com>
2020-11-20 18:06:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,24 +4,31 @@ var port = 4444;
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-
+var online = 0;
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'public'))
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'html')
 
+app.use(function(req,res){
+    res.status(404).render('404page.html');
+});
 app.use('/', (req, res) => {
     res.render('index.html')
+    
 })
+
 
 let messages = [];
 
 io.on('connection', socket => {
+    online++;
+    socket.broadcast.emit('online', online)
+    console.log(online)
 console.log(`socket  conectado id: ${socket.id }`)
 socket.emit('previousMessage', messages)
 socket.on('sendMessage', data => {
     messages.push(data)
-    console.log(messages)
     socket.broadcast.emit('receivedMessage', data)
 })
 })
