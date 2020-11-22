9d34115d02259328c6ba31df4a1270c2b5c7b8fd
ick958 <57007684+ick958@users.noreply.github.com>
2020-11-22 09:38:55 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-var port = 80;
+var port = 8080;
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
