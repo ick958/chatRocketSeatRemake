207f5d807a10f7b1f2f8b3eeca3e50f6ac7fe36b
ick958 <ickmschreiber@gmail.com>
2021-01-03 10:10:29 -0300

    files

--- server.js
+++ server.js
@@ -4,12 +4,27 @@ var port = 3000;
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const multer = require('multer')
 var online = 0;
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'public'))
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'html')
-
+const storage = multer.diskStorage({ destination: function (req, file, cb) {
+    cb(null,"./public")
+},
+filename: (req, file, cb) => {
+    var name = Date.now() + '-' + file.originalname
+    io.on('connection', (socket) => {
+        socket.broadcast.emit('imgR', name)
+    })
+     cb(null, name)
+}
+})
+const uploads = multer({storage})
+app.post('/upload', uploads.single("file"), (req, res) => {
+    res.status(201).send('<script>window.location.href = "/"</script>')
+})
 app.use(function(req,res){
     res.status(404).render('404page.html');
 });
@@ -17,7 +32,9 @@ app.use('/', (req, res) => {
     res.render('index.html')
     
 })
-
+app.get('/favicon.ico', (req, res) => {
+    res.render('favicon.ico')
+})
 
 let messages = [];
 
